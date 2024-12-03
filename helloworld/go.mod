c679addb9805437703249a77fd8274129fde9b3a
Thet Lwin Lwin <minthantlwin8@gmail.com>
2024-12-03 20:44:42 +0630

    if and switch

--- helloworld/go.mod
+++ helloworld/go.mod
@@ -2,14 +2,6 @@ module example.com/helloworld
 
 go 1.23.2
 
-require (
-	com.example/calculator v0.0.0
-	rsc.io/quote v1.5.2
-)
-
-require (
-	golang.org/x/text v0.0.0-20170915032832-14c0d48ead0c // indirect
-	rsc.io/sampler v1.3.0 // indirect
-)
+require com.example/calculator v0.0.0
 
 replace com.example/calculator => ../calculator
--- helloworld/helloworld.go
+++ helloworld/helloworld.go
@@ -2,13 +2,69 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 
 	"com.example/calculator"
-	"rsc.io/quote"
 )
+
+func gimmenumber() int {
+	return -1
+}
 
 func main() {
 	result := calculator.Sum(1, 3)
-	fmt.Println(result)
-	fmt.Println(quote.Hello())
+	if num := result - gimmenumber(); num < 0 {
+		fmt.Println(num, "is negative")
+	} else if num < 10 {
+		fmt.Println(num, "has only one digit left")
+	} else {
+		fmt.Println(num, "has multiple digits")
+	}
+	// outside of the if scope
+	// fmt.Println(num)
+
+	i := rand.Int31n(10)
+
+	switch i {
+	case 1:
+		fmt.Println("One")
+	case 2:
+		fmt.Println("Two")
+	// default is optional
+	default:
+		fmt.Println("Default")
+	}
+	fmt.Println("ok")
+
+	region, continent := location("Irvine")
+	fmt.Printf("John works in %s, %s\n", region, continent)
+	invoker()
+
+}
+
+func location(city string) (string, string) {
+	var region string
+	var continent string
+	switch city {
+	case "Delhi", "Hyderabad", "Mumbai", "Chennai", "Kochi":
+		region, continent = "India", "Asia"
+	case "Lafayette", "Louisville", "Boulder":
+		region, continent = "Colorado", "USA"
+	case "Irvine", "Los Angeles", "San Diego":
+		region, continent = "California", "USA"
+	default:
+		region, continent = "Unknown", "Unknown"
+	}
+	return region, continent
+}
+
+// switch can also invoke a function
+func invoker() {
+	switch time.Now().Weekday().String() {
+	case "Monday", "Tuesday", "Wednesday", "Thursday", "Friday":
+		fmt.Println("It's time to learn Go")
+	default:
+		fmt.Println("Time to rest")
+	}
 }
